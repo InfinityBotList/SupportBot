59eed839c0f5d763aebc15a1ba98d98644524350
TheRealToxicDev <toxic.dev09@gmail.com>
2023-02-10 23:33:34 -0700

    feat(add): Logger usage

---
+++ source/plugins/tests/usage.js
@@ -0,0 +1,21 @@
+const { log } = require("../logger/index");
+
+log("Info message", {
+  header: "Info Header",
+  type: "info",
+});
+
+log("Warning message", {
+  header: "Warning Header",
+  type: "warning",
+});
+
+log("Success message", {
+  header: "Success Header",
+  type: "success",
+});
+
+log("Error message", {
+  header: "Error Header",
+  type: "error",
+});
