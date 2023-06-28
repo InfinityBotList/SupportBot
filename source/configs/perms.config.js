ff26eaf3354b6a851406ec53579e3a7e78da101d
Toxic Dev <59587139+TheRealToxicDev@users.noreply.github.com>
2023-06-28 11:53:26 -0600

    Update perms.config.js

--- source/configs/perms.config.js
+++ source/configs/perms.config.js
@@ -1,8 +1,20 @@
+/**
+ * PERMISSIONS CONFIG FOR COMMANDS
+ * SHOULD BE UPDATED WITH TEAM MEMBER CHNAGE
+ *
+ * Admins = Infinity Owners or Head Devs
+ * Managers = Infinity Staff Managers
+ * Moderators = Infinity Website Mods
+ *
+*/
+
 module.exports = {
-  /** USED FOR COMMAND PERMISSIONS */
   Admins: [
-    "510065483693817867", 
-    "896951964234043413",
-    "728871946456137770"
+    "510065483693817867", // Toxic
+    "896951964234043413", // Elixir
+    "728871946456137770" // Rootspring
+  ],
+  Managers : [
+    "707385680279633970" // Sunrise
   ],
 };
